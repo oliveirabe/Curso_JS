817dccaf377320dde77fe38736def07f497b7514
oliveirabe <oliveirazir@gmail.com>
2021-01-04 14:31:35 -0300

    Aula01 - Variaveis

---
+++ AULA 01.js
@@ -0,0 +1,48 @@
+let cor = 'azul'
+const idade = 26
+
+console.log(cor,idade)
+
+const familia = ['pai', 'mae', 'filho', 'cachorro']
+let ano = 2021
+const frase = 'Feliz ano novo!'
+const casa = {
+    quartos: 4,
+    banheiros: 4,
+    garagem: 'sim',
+    varanda: 'sim'
+}
+
+console.log(familia, ano, frase, casa)
+
+const carro = {
+    portas: 4,
+    cor: 'azul',
+    ano: 2019,
+    modelo: 'Hb20',
+    flex: 'sim',
+    dono: {
+        nome: 'Beatriz',
+        idade: 26,
+        endereco: {
+            rua: 'Rua dos bobos',
+            numero: 0,
+            bairro: 'zerozerozero',
+            cidade: 'Bobolândia'
+        }
+    }
+}
+
+console.log(carro)
+
+const estadosBrasil = ['Acre', 'Alagoas', 'Amapa']
+console.log(estadosBrasil)
+
+let nome1 = 'Beatriz'
+let nome2 = 'Giovanni'
+let nome3 = 'Tânia'
+let nome4 = 'Nathan'
+
+console.log(`Meu nome é ${nome2}, 
+minha namorada é ${nome1}, ${nome3} é minha sogra 
+e ${nome4} é meu cunhado.`) //templateString
